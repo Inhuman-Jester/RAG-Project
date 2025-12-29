9b9a00f4d9c4bf7b3bd9ce8408f0acaf9edf94f7
Inhuman <hj29official@gmail.com>
2025-12-30 01:02:42 +0530

    Added memory to the model, retrieves context from prev Q&A along with a new UI

---
+++ utils/config.py
@@ -0,0 +1,31 @@
+
+import os
+import logging
+from dotenv import load_dotenv
+from pinecone import Pinecone
+from sentence_transformers import SentenceTransformer
+
+# Load environment variables from .env
+load_dotenv(dotenv_path=".env")
+
+# Load API Keys safely
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
+LANGSMITH_PROJECT = os.getenv("LANGSMITH_PROJECT")
+LANGSMITH_TRACING_V2 = os.getenv("LANGSMITH_TRACING_V2")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Set the logging level for httpx, pinecone, pincone_plugin_interface to WARNING to suppress INFO-level logs
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("pinecone").setLevel(logging.WARNING)
+logging.getLogger("pinecone_plugin_interface").setLevel(logging.WARNING)
+
+# Defining the Embedding model to be used for embedding a user query
+embeddings_model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# Initialize Pinecone client and index
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index_name = "testprojectv2"
+index = pc.Index(index_name)
